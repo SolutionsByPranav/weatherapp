fb8341f36a9903019e3d1fb778ee5377c7ceed06
solutionsbypranav <pranavuniyalofficial@gmail.com>
2026-01-14 14:46:39 -0600

    Initial Commit

---
+++ weatherapp/src/test/java/com/portfolio/weatherapp/WeatherappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.weatherapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
